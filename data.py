35786c1a6516a5d708a315d44a7187fcd7358cab
Krzysztof <35111684+kjurkiew@users.noreply.github.com>
2018-03-28 23:08:43 +0200

    Add files via upload

---
+++ data.py
@@ -0,0 +1,19 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title': 'Article One',
+            'body': 'lorem ipsum....',
+            'author': 'Krzysztof Jurkiewicz',
+            'create_date': '14-03-2018'
+        },
+
+        {
+            'id': 2,
+            'title': 'Article two',
+            'body': 'lorem ipsum....',
+            'author': 'Krzysztof Krzyszkiewisz',
+            'create_date': '14-03-2018'
+        }
+    ]
+    return articles
